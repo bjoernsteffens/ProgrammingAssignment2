a9deb4e44599f1c11a06fa7e1bd9b0fcb2ba4da7
Bjoern W. Steffens <bjoern.steffens1@gmail.com>
2015-11-18 08:38:58 +0100

    Adding the first set of modified files

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,73 @@
+# ========================================================================
 ## Put comments here that give an overall description of what your
 ## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
+# ========================================================================
 
 
-## Write a short comment describing this function
+## mm <- makeCacheMatrix (matrix(1:4,nrow=2,ncol=2))
 
+# ========================================================================
+# Function :: cacheSove
+#
+# This function takes a matrix x with any rows||cols and returns 
+# the inverted matrix b< reversing the rows||cols.
+# ========================================================================
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    # initiate the inverse matrix of x with NAs
+    m_tmp <- matrix(NA,nrow=ncol(x), ncol=nrow(x))
+    
+    v_row = 1
+    v_col = 1
+    
+    # Loop through all the columns of x in reverse order
+    for ( k in ncol(x):1) {
+        
+        # Loop through all the rows of x in reverse order
+        # and assign the inverted value to m_tmp
+        #message("Next column ...")
+        
+        v_row = 1
+        for ( r in nrow(x):1) {
+            message("Next row ...")
+            # do the magic here, simply swap the values
+            m_tmp[k,r] <- x[v_row,v_col]
+            #m_tmp[k,r] <- x[r,k]
+            #message(v_col)
+            #message(v_row)
+            v_row <- v_row + 1
+        }
+        v_col <- v_col + 1
+    }
+    m_tmp
 }
+
+# ========================================================================
+# Function :: makeCacheMatrix
+#
+# This function takes a matrix x with any rows||cols and returns 
+# the inverted matrix b< reversing the rows||cols.
+# ========================================================================
+makeCacheMatrix <- function ( x = matrix, ...) {
+    
+    # assign the new matrix passed in and return it.
+    set <- function(y) {
+        message("Assigning a new matrix ...")
+        x <<- y
+    }
+    get <- function() x
+    setinv <- function(y) {
+        message("Assigning a new inverted matrix ...")
+        x <<- y
+    } 
+    getinv <- function() {
+        message("Get the invert of the matrix ...")
+        x <- cacheSolve(x)
+        x
+    }
+    
+    list (set = set, get = get, setinv = setinv, getinv = getinv)
+}
+
+
+
